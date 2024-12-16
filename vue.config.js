88a15d7f297afecd06c923f4c3701b4c1d0b28cf
t-rakuyama <t.rakuyama929@gmail.com>
2024-12-16 10:58:57 +0900

    設定を調整

--- vue.config.js
+++ vue.config.js
@@ -8,6 +8,12 @@ module.exports = defineConfig({
 
   outputDir: 'docs',
 
+  configureWebpack: {
+    devServer: {
+      historyApiFallback: true
+    }
+  },
+
   pluginOptions: {
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
